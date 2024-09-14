bb45bd64df360663b94014dd19315672b61327c6
YashSingh00987 <mohitsingh00987@gmail.com>
2024-09-14 19:44:57 +0530

    first push

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement(
+    "div",
+    {id:"root"},
+    React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1",{},"Hello from H1 Tag"),
+            React.createElement("h2",{},"Hello from H2 Tag")
+        ]
+    )
+)
+
+
